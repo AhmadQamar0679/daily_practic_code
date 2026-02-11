aefb4b796a33b57e19b86aec702ed96c18b8aefd
Ahmad Qamar <ahmadqamar.rq@gmail.com>
2026-02-11 07:16:46 +0500

    initial structure

---
+++ a_main/b_practic/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render,redirect
+
+from .models import *
+
+# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        data=request.POST
+        first_name=data.get("first_name")
+        last_name=data.get("last_name")
+        Age=data.get("Age")
+        print("first_name: ",first_name)
+        print("last_name: ",last_name)
+        print("Age: ",Age)
+    
+        User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            Age=Age,
+            
+
+        )
+        return redirect("/")
+    
+    return render(request,'home.html')
